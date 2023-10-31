b819a60b2de829673c7a5b861e4a5561a7a64ccd
Gustanto <gustanto.mucharom@rschooltoday.com>
2023-10-31 14:30:19 +0700

    follow unfollow

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -8,6 +8,8 @@ import { IconHoverEffect } from "~/components/IconHoverEffect";
 import { VscArrowLeft } from "react-icons/vsc";
 import { ProfileImage } from "~/components/ProfileImage";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
+import { Button } from "~/components/Button";
+import { useSession } from "next-auth/react";
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ id }) => {
     const {data: profile} = api.profile.getById.useQuery({ id })
@@ -15,6 +17,22 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         { userId: id },
         { getNextPageParam: (lastPage) => lastPage.nextCursor }
     )
+
+    const trpcUtils = api.useContext()
+    const toggleFollow = api.profile.toggleFollow.useMutation({
+        onSuccess: ({ addedFollow }) => {
+            trpcUtils.profile.getById.setData({ id }, oldData => {
+                if (oldData == null) return
+
+                const countModifier = addedFollow ? 1 : -1
+                return {
+                    ...oldData,
+                    isFollowing: addedFollow,
+                    followersCount: oldData.followersCount + countModifier
+                }
+            })
+        }
+    })
 
     if (profile == null || profile.name == null) {
         return <ErrorPage statusCode={404} />
@@ -45,8 +63,9 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                 </div>
                 <FollowButton
                     isFollowing={profile.isFollowing}
+                    isLoading={toggleFollow.isLoading}
                     userId={id}
-                    onClick={() => null}
+                    onClick={() => toggleFollow.mutate({ userId: id})}
                     />
             </header>
             <main>
@@ -62,8 +81,26 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
     )
 }
 
-function FollowButton() {
-    return <h1>Follow</h1>
+function FollowButton({
+    userId,
+    isLoading,
+    isFollowing,
+    onClick
+}: {
+    userId: string
+    isLoading: boolean
+    isFollowing: boolean
+    onClick: () => void
+}) {
+    const session = useSession()
+
+    if (session.status !== "authenticated" || session.data.user.id === userId) {
+        return null
+    }
+
+    return <Button disabled={isLoading} onClick={onClick} small gray={isFollowing}>
+        {isFollowing ? "unfollow" : "follow"}
+    </Button>
 }
 
 const pluralRules = new Intl.PluralRules()

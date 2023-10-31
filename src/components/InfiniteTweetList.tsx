16d77d1744caed19b6306891a528caa2d8348ef8
Gustanto <gustanto.mucharom@rschooltoday.com>
2023-10-31 11:35:59 +0700

    Recent and Following Tabs

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react"
 import { VscHeart, VscHeartFilled } from "react-icons/vsc"
 import { IconHoverEffect } from "./IconHoverEffect"
 import { api } from "~/utils/api"
+import { LoadingSpinner } from "./LoadingSpinner"
 
 type Tweet = {
     id: string
@@ -28,9 +29,9 @@ export function InfiniteTweetList({
     isError,
     isLoading,
     fetchNewTweets,
-    hasMore 
+    hasMore = false,
 }: InfiniteTweetListProps) {
-    if (isLoading) return <h1>Loading...</h1>
+    if (isLoading) return <LoadingSpinner />
     if (isError) return <h1>Error...</h1>
 
     if (tweets == null || tweets.length === 0) {
@@ -45,7 +46,7 @@ export function InfiniteTweetList({
                 dataLength={tweets.length}
                 next={fetchNewTweets}
                 hasMore={hasMore}
-                loader={"Loading..."}
+                loader={<LoadingSpinner />}
             >
                 {tweets.map(tweet => {
                     console.log('tweet', tweet)
